fcee39dc1e589024a46fd8e54fd0087000b2e750
chanble <chanble_cn@163.com>
2019-09-23 16:20:34 +0800

    Create md5Hash.php

---
+++ md5Hash.php
@@ -0,0 +1,9 @@
+
+    public static function md5Hash($password, $salt, $iterations)
+    {
+        $hash = hash('md5', $salt.$password, TRUE);
+        for ($i = 0; $i < $iterations - 1; $i++) {
+            $hash = hash('md5', $hash, TRUE);
+        }
+        return bin2hex($hash);
+    }
